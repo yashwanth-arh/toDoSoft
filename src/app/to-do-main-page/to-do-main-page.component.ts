3b1fe2ffeb179d5a81dc04bb600d9eeeb984f075
AnubhavU <anubhav.utkarsh@xyramsoft.com>
2022-05-28 22:07:02 +0530

    main page

--- src/app/to-do-main-page/to-do-main-page.component.ts
+++ src/app/to-do-main-page/to-do-main-page.component.ts
@@ -1,15 +1,149 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-to-do-main-page',
+//   templateUrl: './to-do-main-page.component.html',
+//   styleUrls: ['./to-do-main-page.component.scss']
+// })
+// export class ToDoMainPageComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
+
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-to-do-main-page',
   templateUrl: './to-do-main-page.component.html',
-  styleUrls: ['./to-do-main-page.component.scss']
+  styleUrls: ['./to-do-main-page.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(30px)' }),
+        animate(200, style({ opacity: 1, transform: 'translateY(0px)' })),
+      ]),
+
+      transition(':leave', [
+        animate(200, style({ opacity: 0, transform: 'translateY(30px)' })),
+      ]),
+    ]),
+  ],
 })
 export class ToDoMainPageComponent implements OnInit {
+  todos: Todo[];
+  todoTitle: string;
+  idForTodo: number;
+  beforeEditCache: string;
+  filter: string;
+  anyRemainingModel: boolean;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.anyRemainingModel = true;
+    this.filter = 'all';
+    this.beforeEditCache = '';
+    this.idForTodo = 4;
+    this.todoTitle = '';
+    this.todos = [
+      {
+        id: 1,
+        title: 'Finish Angular Screencast',
+        completed: false,
+        editing: false,
+      },
+      {
+        id: 2,
+        title: 'Take over world',
+        completed: false,
+        editing: false,
+      },
+      {
+        id: 3,
+        title: 'One more thing',
+        completed: false,
+        editing: false,
+      },
+    ];
   }
 
+  addTodo(): void {
+    if (this.todoTitle.trim().length === 0) {
+      return;
+    }
+
+    this.todos.push({
+      id: this.idForTodo,
+      title: this.todoTitle,
+      completed: false,
+      editing: false,
+    });
+
+    this.todoTitle = '';
+    this.idForTodo++;
+  }
+
+  editTodo(todo: Todo): void {
+    this.beforeEditCache = todo.title;
+    todo.editing = true;
+  }
+
+  doneEdit(todo: Todo): void {
+    if (todo.title.trim().length === 0) {
+      todo.title = this.beforeEditCache;
+    }
+
+    this.anyRemainingModel = this.anyRemaining();
+    todo.editing = false;
+  }
+
+  cancelEdit(todo: Todo): void {
+    todo.title = this.beforeEditCache;
+    todo.editing = false;
+  }
+
+  deleteTodo(id: number): void {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
+
+  remaining(): number {
+    return this.todos.filter((todo) => !todo.completed).length;
+  }
+
+  atLeastOneCompleted(): boolean {
+    return this.todos.filter((todo) => todo.completed).length > 0;
+  }
+
+  clearCompleted(): void {
+    this.todos = this.todos.filter((todo) => !todo.completed);
+  }
+
+  checkAllTodos(): void {
+    this.todos.forEach(
+      (todo) => (todo.completed = (<HTMLInputElement>event.target).checked)
+    );
+    this.anyRemainingModel = this.anyRemaining();
+  }
+
+  anyRemaining(): boolean {
+    return this.remaining() !== 0;
+  }
+
+  todosFiltered(): Todo[] {
+    if (this.filter === 'all') {
+      return this.todos;
+    } else if (this.filter === 'active') {
+      return this.todos.filter((todo) => !todo.completed);
+    } else if (this.filter === 'completed') {
+      return this.todos.filter((todo) => todo.completed);
+    }
+
+    return this.todos;
+  }
 }
