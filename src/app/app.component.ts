980156545338b66af663949a621c2ecf9d1600a6
AnubhavU <anubhav.utkarsh@xyramsoft.com>
2022-05-30 21:49:58 +0530

    new ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArrayList } from '@arjunatlast/jsds';
+import { Todo } from './api/todo';
+
+import { ToDoService } from './service/to-do.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'toDoSoft';
+export class AppComponent implements OnInit {
+  todoList: ArrayList<Todo>;
+  selectedTodos: ArrayList<Todo>;
+  todoTitle: string;
+  todoContent: string;
+
+  constructor(private ts: ToDoService) {}
+
+  ngOnInit() {
+    this.todoList = this.ts.getTodos();
+    this.selectedTodos = this.ts.getSelectedTodos();
+  }
+
+  addTodo(): void {
+    this.ts.addTodo(new Todo(this.todoTitle, this.todoContent));
+  }
+
+  deleteTodos(): void {
+    this.ts.removeSelectedTodos();
+  }
+
+  openFile(): void {
+    document.getElementById('backup_file').click();
+  }
 }
